89818dde28cc79c83e7bf001509260db9cb86877
Sushant Verma <54903513+iamstyx@users.noreply.github.com>
2022-12-27 00:36:03 +0530

    Add Some Files

---
+++ Practice_Set2.java
@@ -0,0 +1,48 @@
+import  java.util.Scanner;
+public class Practice_Set2 {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+/*        // 1. What Will be the result of a = 7/4 8 9/2
+            float a = 7/4.0f * 9/2.0f;
+            System.out.println(a);*/
+
+/*          // Write a program to encrypt a grade by adding 8 to it. Decrypt it to show the correct grade
+
+            char grade = 'B';
+            grade = (char) (grade + 8);
+            System.out.println(grade);
+
+            // Decrypt The Grade
+            grade = (char) (grade - 8);
+            System.out.println(grade);*/
+
+/*          // Use Comparison Operator to find out whether a given number is greater than the user entered number or not
+
+            int a = sc.nextInt();
+            System.out.println(a>8);*/
+
+/*
+            // Write the following expression in a java program
+            // V2-u2/2as
+
+            float v=25.0f;
+            float u=10.0f;
+            float a=12.0f;
+            float s = 5.0f;
+            float formula=(v*v-u*u)/(2*a*s);
+            System.out.println(formula);
+*/
+
+/*            // Find The Value of the following expression
+            // int x = 7
+            // int a = 7*49/7 + 35/7
+            // find the value of a ?
+
+            int x = 7;
+            int a = 7*49/7 + 35/7;
+            System.out.println(a);*/
+
+        }
+    }
---
+++ Resulting_DataType.java
@@ -0,0 +1,41 @@
+public class Resulting_DataType {
+    public static void main(String[] args) {
+        /* byte x = 5;
+        int y = 6;
+        short z = 8;
+        int a = y + z;
+        float b = 6.54f + x;
+        System.out.println(b); */
+
+        // Increment and Decrement Operators
+/*        int i = 56;
+        // int b = i++; // first b is assigned i (56) then i is incremented
+        int j = 67;
+        int c = ++j; // first j is incremented then c is assigned j (68)
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        int y = 7;
+        System.out.println( ++y *8);
+        char ch = 'a';
+        System.out.println(++ch);*/
+
+/*        // Quiz
+
+        int a = 60;
+        int b = 50;
+
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(--b);
+        System.out.println(++b);*/
+
+        // Quiz 2
+
+        int y = 7;
+        int x = ++y *8 ;
+
+        System.out.println(x);
+    }
+}
